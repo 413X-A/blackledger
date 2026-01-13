47845a5a32b174944ba14b276118304a48878a20
413X-A <wollekalexander@gmail.com>
2026-01-13 01:17:51 +0100

    Delete animations.js

--- animations.js
+++
@@ -1,9 +0,0 @@
-const Animations = (() => {
-  function highlightPanel(id){
-    const el = document.getElementById(id);
-    if(!el) return;
-    el.animate([{boxShadow:"0 0 0 gold"},{boxShadow:"0 0 15px gold"}],{duration:500,iterations:2});
-  }
-
-  return {highlightPanel};
-})();
