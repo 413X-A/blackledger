4384c1b5e7b8646c39f42869af1ce62acc0d7132
413X-A <wollekalexander@gmail.com>
2026-01-13 01:16:19 +0100

    Update research.js

--- research.js
+++ research.js
@@ -1,26 +1,10 @@
 const Research = (() => {
-  let tech = {
-    buchhaltung:false,
-    versicherung:false,
-    boerse:false
-  };
+  const tech = {bookkeeping:false, insurance:false, exchange:false};
 
   function forschen(name){
-    if(!tech[name]){
-      tech[name] = true;
-      UI.showOverlay(`Forschung abgeschlossen: ${name}!`);
-      return true;
-    } else {
-      UI.showOverlay(`${name} ist bereits erforscht!`);
-      return false;
-    }
+    tech[name]=true;
+    UI.log(`Forschung abgeschlossen: ${name}`);
   }
 
-  function updateUI(){
-    document.getElementById("tech-bookkeeping").textContent = tech.buchhaltung ? "Fertig" : "Gesperrt";
-    document.getElementById("tech-insurance").textContent = tech.versicherung ? "Fertig" : "Gesperrt";
-    document.getElementById("tech-exchange").textContent = tech.boerse ? "Fertig" : "Gesperrt";
-  }
-
-  return {tech,forschen,updateUI};
+  return {tech,forschen};
 })();
